e293c2a0f0ae6141e8fa00024ec8cccfb68976c0
nenadpetrovikj <86932367+nenadpetrovikj@users.noreply.github.com>
2024-01-27 15:14:09 +0100

    Remove commented code

--- src/main/java/mk/ukim/finki/wp/project/ednevnik/EdnevnikApplication.java
+++ src/main/java/mk/ukim/finki/wp/project/ednevnik/EdnevnikApplication.java
@@ -2,10 +2,8 @@ package mk.ukim.finki.wp.project.ednevnik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class EdnevnikApplication {
 
     public static void main(String[] args) {
